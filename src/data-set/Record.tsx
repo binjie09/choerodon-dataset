b80454496717651b74f40bde0bdb8a61bb377e66
陈聪 <cong.chen02@hand-china.com>
2021-04-06 19:09:44 +0800

    Update src/data-set/Record.tsx

--- src/data-set/Record.tsx
+++ src/data-set/Record.tsx
@@ -66,7 +66,7 @@ export default class Record {
   get pristineData(): object {
     const dirtyData = {};
     [...this.dirtyData.entries()].forEach(([key, value]) => ObjectChainValue.set(dirtyData, key, value));
-    return merge({}, {}, dirtyData);
+    return merge({}, this.data, dirtyData);
   }
 
   set pristineData(data: object) {
